4dc027656ca05a2dfa171acf15b2eb9fbbc4cd9f
Alex Lübeck <ndsvw@gmx.de>
2020-01-31 08:49:00 +0100

    implemented space-improved algorithm

--- min_sum_partitioning.py
+++ min_sum_partitioning.py
@@ -10,3 +10,20 @@ def min_sum_first_attempt(arr):
                 if matrix[i-1][j+arr[i]] <= matrix[i][j]:
                     matrix[i][j] = matrix[i-1][j+arr[i]]
     return matrix[n-1][0]
+
+
+def min_sum_space_improved(arr):
+    n = len(arr)
+    sum_of_all = sum(arr)
+    matrix = [
+        [abs(sum_of_all - i - i) for i in range(sum_of_all+1)],
+        [None for _ in range(sum_of_all+1)]
+    ]
+    for i in range(1, n):
+        for j in range(sum_of_all, -1, -1):
+            matrix[1][j] = matrix[0][j]
+            if j+arr[i] <= sum_of_all:
+                if matrix[0][j+arr[i]] <= matrix[1][j]:
+                    matrix[1][j] = matrix[0][j+arr[i]]
+        matrix[0] = [e for e in matrix[1]]
+    return matrix[1][0]
